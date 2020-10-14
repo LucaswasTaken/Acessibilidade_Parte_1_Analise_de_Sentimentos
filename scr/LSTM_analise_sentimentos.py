55e81412d5807cfc1cd668a293012d6c9935cd79
Lucas lopes <lucasomena@lccv.ufal.br>
2020-10-14 09:02:15 -0300

    Fatoração do código

--- scr/LSTM_analise_sentimentos.py
+++ scr/LSTM_analise_sentimentos.py
@@ -8,23 +8,24 @@ from  tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 import numpy as np
 
-
+#Definição da classe que realiza a previsão
 class Prediction():
+    #construtor
     def __init__(self,tokenizer,max_len):
         self.model = None
-        self.tokenizer = tokenizer
-        self.idx2word = {v:k for k,v in self.tokenizer.word_index.items()}
-        self.max_length = max_len+1
-    
+    #leitura do modelo treinado(google colab)
     def load_model(self):
         self.model = load_model("class_prediction_model.h5")
-        
+    #pad-sequencias e previsão    
     def predict_words(self,encoded_data):
         encoded_data = pad_sequences(encoded_data,maxlen = 11 ,padding='pre')
         y_preds = self.model.predict(encoded_data)
         return y_preds
 
+#Leitura do texto a ser avaliado
 text = input('digite o texto a ser avaliado: ')
+
+#preprocessamento e limpeza do texto para remover informações espurias
 text = unidecode(text.lower())
 text= re.sub(r'\[[0-9]*\]', ' ', text)
 text = re.sub(r'\s+', ' ', text)
@@ -33,16 +34,18 @@ text = re.sub(r'\s+', ' ', text)
 text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
 clean_text = [text]
 
+#tokenização do texto com base no dicionário de treino da rede
 with open('tokenizer.pickle', 'rb') as handle:
   tokenizer = pickle.load(handle)
 
 encoded_data = tokenizer.texts_to_sequences(clean_text)
 
+#inicialização da classe de previsão
 max_length = 11
-
 pred = Prediction(tokenizer, max_length)    
 pred.load_model()
 
+#previsão e determinação do sentimento
 y_pred = pred.predict_words(encoded_data)
 if(np.argmax(y_pred)==0):
   y_pred_class='raiva'
@@ -50,4 +53,5 @@ elif(np.argmax(y_pred)==1):
   y_pred_class='tristeza'
 else:
   y_pred_class='alegria'
+#impressaço na tela
 print('O sentimento é: ', y_pred_class)
